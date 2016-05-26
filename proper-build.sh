76c33bc7a393aa6239afe75482f2a1e34e7c8082
Shankie <andrew@properdesign.rs>
2016-05-26 09:51:26 +0100

    Adds build script

--- proper-build.sh
+++ proper-build.sh
@@ -4,7 +4,7 @@
 # TMPDIR=$(dirname $(mktemp -u)) && \
 #   curl -sLo "$TMPDIR/properbuild.zip" "https://github.com/shankiesan/proper-build/archive/master.zip" && \
 #   unzip -jq -o "$TMPDIR/properbuild.zip" -d $TMPDIR && \
-#   cp "$TMPDIR/proper-build-master/proper-build.sh" /usr/local/bin/proper-build
+#   cp "$TMPDIR/proper-build.sh" /usr/local/bin/proper-build
 
 # Pull the latest image if we're asked
 if [ "$1" == "pull" ]; then 
