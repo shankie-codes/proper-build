6c4d38cef75042b65fa914cfe808c723aebd5ce0
Shankie <andrew@properdesign.co.uk>
2016-11-28 14:15:15 +0000

    Adds ES7 stage 0

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,10 +1,12 @@
+// var module_exists = require('module-exists');
+
 module.exports = [
 	{
 		test: /\.jsx?$/,
 		exclude: /(node_modules|bower_components)/,
 		loader: require.resolve('babel-loader'),
 		query: {
-		  presets: [require.resolve('babel-preset-es2015'), require.resolve('babel-preset-react')],
+		  presets: [require.resolve('babel-preset-es2015'), require.resolve('babel-preset-react'), require.resolve('babel-preset-stage-0')],
 		  // plugins: [require.resolve('babel-plugin-transform-runtime'), require.resolve('babel-plugin-transform-decorators-legacy'), require.resolve('babel-plugin-transform-class-properties')],
 		  plugins: [require.resolve('babel-plugin-transform-decorators-legacy'), require.resolve('babel-plugin-transform-class-properties'), require.resolve('babel-plugin-transform-object-rest-spread')],
 		}
@@ -57,3 +59,11 @@ module.exports = [
 		loader: "url-loader?limit=10000&mimetype=image/png"
 	}
 ];
+
+// Hack to get AMD-moduled snapsvg working
+// if (module_exists('snapsvg')) {
+// 	module.exports.push({
+//         test: require.resolve('snapsvg'),
+//         loader: 'imports-loader?this=>window,fix=>module.exports=0'
+//       });
+// }
