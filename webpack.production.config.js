e62aeccd3228f0921d76dd74c3dfc1387770e3cd
Shankie <andrew@properdesign.co.uk>
2017-03-29 10:33:50 +0100

    removes modernizr

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -43,7 +43,6 @@ module.exports = {
 		alias: {
         // 'jquery': '/build/node_modules/jquery/dist/jquery.min.js',
         // '$': '/build/node_modules/jquery/dist/jquery.min.js',
-        modernizr$: path.resolve(__dirname, ".modernizrrc")
     },
     root : ['/build', '/source/'],
 	},
